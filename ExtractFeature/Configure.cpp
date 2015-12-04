6c6ed41a5c314d9931e592ad320c9b23aacf3ecc
安兴乐-siler <anxingle820@gmail.com>
2015-12-04 12:30:48 +0800

    ??

---
+++ ExtractFeature/Configure.cpp
@@ -0,0 +1,33 @@
+#include "Configure.h"
+
+
+const std::string trainPath = "K:\\qcz\\AliSearch\\train_image\\Competition";
+const std::string testPath = "K:\\qcz\\AliSearch\\train_image\\Competition";
+
+const int surfDescriptorSize = 64;
+const int surfBlockSize = 100;
+const int denseSurfFeatureSize = surfBlockSize * surfDescriptorSize;
+const int clusterCount = 128;
+const int surfVladFeatureSize = clusterCount * surfDescriptorSize; 
+const int featureAfterPCASize = 256;
+const int nearestK = 100;
+//const int nbits = 256;
+const int categoryNum = 559;
+
+FEAFLAGS feaflags = DENSESURF;
+float** vocabulary = 0;
+float** pcaMatrix = 0;
+float** rotationMatrix = 0;
+
+std::unordered_map<std::string, std::vector<float*> > trainFeatureProVladMap;
+
+std::unordered_map<std::string, std::vector<float*> > trainFeatureDenseSurfMap;
+
+std::vector<QueryRe > IRResult;
+std::vector<cv::Mat > QueryPro;
+
+std::vector<double > SimRe;
+std::vector<std::string > ImgName;
+std::vector<cv::Mat > ShowImgs;
+int pageIdx = 1;
+const int ImgNumPerPage = 3;
