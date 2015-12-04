5141f64f0d0b53cfa4104c4af085ed4007acb521
安兴乐-siler <anxingle820@gmail.com>
2015-12-04 12:21:07 +0800

    new project by zhangqi!

--- search_project/LoadFile.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOAD_FILE_H_
-#define _LOAD_FILE_H_
-#include <string>
-using namespace std;
-
-void load(string vocabularyFile, string imgPathProVladFile, string trainFeatureProVladFile,string imgPathDenseSurfFile,string trainFeatureDenseSurfFile);
-
-#endif
